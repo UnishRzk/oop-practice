01aed14238d08799b50d72c5925e39f2399f8866
Unish Rajak <unishrajak0@gmail.com>
2025-06-11 19:57:04 +0545

    Nested Interfaces

--- src/com/unish/extendedDemo2/A.java
+++ src/com/unish/extendedDemo2/A.java
@@ -1,6 +1,13 @@
 package com.unish.extendedDemo2;
 
 public interface A {
+
+
+    // static interface methods should always have a body
+    static void greeting(){
+        System.out.println("hey i am static method");
+    }
+
     default void fun(){
         System.out.println("I am in A");
     }
--- src/com/unish/extendedDemo2/Main.java
+++ src/com/unish/extendedDemo2/Main.java
@@ -11,4 +11,11 @@ public class Main implements A,B {
     public void greet() {
 
     }
+
+    public static void main(String[] args) {
+//        Main obj = new Main();
+
+        A.greeting();
+
+    }
 }
